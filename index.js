91a29217464e7bea9527014c0b90df4118f68e94
ldsouza <ldsouza@deloitte.com>
2020-03-07 22:09:43 +0530

    port update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 const cheerio = require("cheerio");
 const axios = require("axios");
 const puppeteer = require('puppeteer');
